310226ce52636fef6cf2cfc4cd00fc570cda1d4d
Marion Le Borgne <leborgne.mar@gmail.com>
2015-10-13 22:54:31 -0700

    EEG encoder + test

--- brainsquared/encoder/eeg_encoder.py
+++ brainsquared/encoder/eeg_encoder.py
@@ -8,64 +8,57 @@ from nupic.encoders.scalar import ScalarEncoder
 
 class EEGEncoder(Encoder):
   """
-  This is an implementation of an EEG encoder. An EEG wave is converted into
-  the maximum frequency detected according to FFT, and this frequency is
-  encoded into an SDR using a ScalarEncoder.
+  This is an implementation of an EEG encoder. A frequency value is 
+  extracted from an EEG wave and is encoded into an SDR using a ScalarEncoder.
   """
 
 
-  def __init__(self, n, w, rate, chunk, minval=20, maxval=20000, name=None):
+  def __init__(self, n, w, rate, chunk, minval, maxval, minFreq, maxFreq, 
+               name=None):
     """
     @param n int the length of the encoded SDR
     @param w int the number of 1s in the encoded SDR
     @param rate int the number of EEG samples per second
     @param chunk int the number of samples in an input
-    @param minval float the lowest possible frequency detected
-    @param maxval float the highest possible frequency detected
+    @param minval float the minval of the encoder
+    @param maxval float the maxval of the encoder
+    @param minFreq float the lowest possible frequency detected
+    @param maxFreq float the highest possible frequency detected
     @param name string the name of the encoder
     """
     self.n = n
     self.w = w
     self.rate = rate
     self.chunk  = chunk
-    self.minval = minval
-    self.maxval = maxval
+    self.minFreq = minFreq
+    self.maxFreq = maxFreq
     self.name = name
     self._scalarEncoder = ScalarEncoder(name="scalar_"+str(name), n=n, w=w,
                                         minval=minval, maxval=maxval)
 
 
-  def _detectFrequency(self, inputArr):
-    """Use FFT to find maximum frequency present in the input."""
-    fftData=abs(np.fft.rfft(inputArr))**2
-    maxFreqIdx = np.argmax(fftData)
-
-    if maxFreqIdx < len(fftData)-1:
-      # Quadratic interpolation
-      y0, y1, y2 = np.log(fftData[maxFreqIdx-1:maxFreqIdx+2:])
-      x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
-      return (maxFreqIdx+x1)*(self.rate/self.chunk)
-
-    # Maximum idx is last in list, so cannot do quadratic interpolation
-    return (maxFreqIdx+x1)*(self.rate/self.chunk)
+  def _getFrequencyAmplitude(self, inputArr):
+    """
+    Use FFT to find maximum frequency present in the input.
+    @param inputArr (list of floats): chunk of data to take the FFT of.
+    """
+    
+    #TODO: maybe take the log or convert to dB instead of just the FFT
+    fftData=abs(np.fft.rfft(inputArr)) # absolute value to get real numbers
+    freqs = np.fft.rfftfreq(len(inputArr), 1.0/self.rate)
+    
+    # Take the mean of the frequency amplitudes that are in the range of the 
+    # min and max frequency. 
+    amps = fftData[np.logical_and(freqs <= self.maxFreq, freqs >= self.minFreq)]
+    return np.mean(amps)
 
 
   def encodeIntoArray(self, inputArr, output):
     if not isinstance(inputArr, (list, np.ndarray)):
       raise TypeError(
           "Expected a list or numpy array but got input of type %s" % type(inputArr))
-
-    if inputArr == SENTINEL_VALUE_FOR_MISSING_DATA:
-      output[0:self.n] = 0
-    else:
-      frequency = self._detectFrequency(inputArr)
-      # Fail fast if frequency is outside allowed range.
-      if (frequency < self.minval) or (frequency > self.maxval):
-        raise ValueError(
-             "Frequency value %f is outside allowed range (%f, %f)" % (
-                  frequency, self.minval, self.maxval))
-
-      output[0:self.n] = self._scalarEncoder.encode(frequency)
+    frequencyAmplitude = self._getFrequencyAmplitude(inputArr)
+    output[0:self.n] = self._scalarEncoder.encode(frequencyAmplitude)
 
 
   def getWidth(self):
