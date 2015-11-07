9a414ed8c8ac7b0cf3e77766a2142d92cf70daff
Marion Le Borgne <leborgne.mar@gmail.com>
2015-11-07 02:03:49 -0800

    testing

---
+++ brainsquared/tests/integration/publish_mock_tag.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from brainsquared.publishers.PikaPublisher import PikaPublisher
+
+USER_ID = "brainsquared"
+MODULE_IDS = ["module0", "module1", "module2", "module3"]
+DEVICE = "openbci"
+
+
+pub = PikaPublisher("rabbitmq.cloudbrain.rocks", "cloudbrain", "cloudbrain")
+pub.connect()
+
+for module_id in MODULE_IDS:
+  TAG_KEY = '%s:%s:tag' % (USER_ID, module_id)
+  pub.register(TAG_KEY)
+  #pub.publish(TAG_KEY, {"timestamp": 1, "value": "middle"})
+  #pub.publish(TAG_KEY, {"timestamp": 1, "value": "left"})
+  #pub.publish(TAG_KEY, {"timestamp": 1, "value": "right"})
+
+
+MU_KEY = '%s:%s:mu' % (USER_ID, DEVICE)
+pub.register(MU_KEY)
+pub.publish(MU_KEY, {"timestamp": 1, "value": 1})
+
+
