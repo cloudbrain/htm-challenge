92183a57144e1f2f67a05d6604a5f9a9012f756d
Pierre Karashchuk <krchtchk@gmail.com>
2015-11-08 19:03:05 -0800

    add notch filter to preprocessing

--- brainsquared/preprocessing/eeg_preprocessing.py
+++ brainsquared/preprocessing/eeg_preprocessing.py
@@ -59,7 +59,7 @@ def stft(X, y=None, box_width=128, step=32, pad_width=0, kaiser_beta=14, include
 
     for start in range(0, m, step):
         end = start + box_width
-        if end >= len(X):
+        if end > len(X):
             break
 
         x = X[start:end,]
@@ -254,11 +254,17 @@ def preprocess_morlet(data, metadata, downsampling_factor=32,
                               data=data, metadata=metadata)
 
 
-def preprocess_stft(data, metadata,
+def preprocess_stft(data, metadata, notch_filter=True,
                     box_width=128, downsampling_factor=32,
                     sfreq=250, low_f=8, high_f=12, kaiser_beta=14):
 
+    b, a = signal.iirfilter(4, [56/250.0, 64/250.0], btype='bandstop')
+
     def do_stft(arr):
+
+        if notch_filter:
+            arr = signal.lfilter(b, a, arr)
+        
         out = stft(arr[:, np.newaxis],
                    box_width=box_width, step=downsampling_factor, pad_width=0,
                    kaiser_beta=kaiser_beta, include_phase=False, log_mag=False)
@@ -270,6 +276,7 @@ def preprocess_stft(data, metadata,
         return out
 
 
+    
     return preprocess_general(process=do_stft,
                               data=data, metadata=metadata)
 
@@ -315,7 +322,10 @@ def read_file_to_buffer(path_to_csv):
             if v == None or v == 'nan':
                 r = False
                 break
-            r[k] = float(v)
+            try:
+                r[k] = float(v)
+            except ValueError:
+                r[k] = v
         if r:
             out.append(r)
 
@@ -359,3 +369,5 @@ def preprocess_morlet_file(path_to_csv, metadata,
 
 
 ## TODO: add ICA stuff to remove eye blinks here
+
+
