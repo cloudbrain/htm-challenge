1bc7ce2ef6e17fe950a3e57835d22b13d5a3edf7
William Wnekowicz <william.wnekowicz@gmail.com>
2016-01-29 00:10:49 -0800

    Add Brainsquared Octopicorn Game

---
+++ brainsquared/frontend/app.js
@@ -0,0 +1,43 @@
+/* global angular */
+(function () { 'use strict';
+
+  var app = angular.module('cloudbrain', [
+  	'ui.bootstrap',
+  	'ui.router',
+  	'ngMaterial',
+  	'highcharts-ng',
+  	'cloudbrain.brainsquared',
+  ]);
+  app.config(function($mdThemingProvider) {
+    $mdThemingProvider.definePalette('cloudbrain', {
+      '50': 'ffebee',
+      '100': 'ffcdd2',
+      '200': 'ef9a9a',
+      '300': 'ffffff',
+      '400': 'ef5350',
+      '500': 'f44336',
+      '600': 'ffffff',
+      '700': 'd32f2f',
+      '800': 'c62828',
+      '900': 'ffffff',
+      'A100': '377BB5',
+      'A200': '377BB5',
+      'A400': '377BB5',
+      'A700': '377BB5',
+      'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+      'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+       '200', '300', '400', 'A100'],
+      'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+
+	  $mdThemingProvider.theme('default')
+	    .primaryPalette('cloudbrain',  {
+         'default': 'A200',
+         'hue-1': '300',
+         'hue-2': '600',
+         'hue-3': '900'
+       })
+	    .accentPalette('pink');
+	});
+})();
