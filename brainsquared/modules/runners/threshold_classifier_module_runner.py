a55dbee2013969bb884765da72986f6cf2bd272e
Marion Le Borgne <mleborgne@numenta.com>
2016-02-03 23:43:03 -0800

    fixed constants

--- brainsquared/modules/runners/threshold_classifier_module_runner.py
+++ brainsquared/modules/runners/threshold_classifier_module_runner.py
@@ -3,17 +3,17 @@ from brainsquared.modules.classifiers.ThresholdClassifier import \
 
 if __name__ == "__main__":
   _USER_ID = "brainsquared"
-  _DEVICE_TYPE = "neurosky"
+  _DEVICE_TYPE = "wildcard"
 
   _RMQ_ADDRESS = "localhost"
   _RMQ_USER = "guest"
   _RMQ_PWD = "guest"
 
   # Module configuration
-  _INPUT_METRICS = {"input": "mindwave", "input_label": "NA"}
+  _INPUT_METRICS = {"input": "motor_imagery", "input_label": "NA"}
   _OUTPUT_METRICS = {"classification_result": "classification"}
-  _ATTENTION_THRESHOLD = 70
-  _MEDITATION_THRESHOLD = 30
+  _ATTENTION_THRESHOLD = 50
+  _MEDITATION_THRESHOLD = 50
   _THRESHOLDS = {
     "channel_0": _MEDITATION_THRESHOLD,
     "channel_1": _ATTENTION_THRESHOLD
