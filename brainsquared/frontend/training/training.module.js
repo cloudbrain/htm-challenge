365a7074e72ca4300179591fae9355ffbfb05b9e
William Wnekowicz <william.wnekowicz@gmail.com>
2016-02-14 00:40:48 -0800

    Add training service for UI

--- brainsquared/frontend/training/training.module.js
+++ brainsquared/frontend/training/training.module.js
@@ -2,7 +2,16 @@
   'use strict';
 
   angular.module('cloudbrain.training', ['ngMaterial'])
-    .constant('MODULE_URL', 'http://brainsquared.apiserver.cloudbrain.rocks/api/v0.1/users/brainsquared/modules')
-    .constant('TRAINING_CLASSIFIER_TIMEOUT', 4000)
+    .constant('TRAINING_URL', 'http://localhost:8080/api/v0.1/modules/start')
+    .constant('TRAINING_CLASSIFIER_TIMEOUT', 10000)
     .constant('COUNTER', 10)
+    .constant('CONFIG', {
+      host: 'localhost',
+      login: 'guest',
+      pwd: 'guest',
+      user: 'brainsquared',
+      device: 'neurosky',
+      metric: 'attention',
+      output: 'classification'
+    })
 })();
